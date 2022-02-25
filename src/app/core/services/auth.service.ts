bdd093541de580481d28a77a149cf2a20d0e9760
Ambroise BAZIE <ambroise.bazie@infisoftware.net>
2022-02-25 12:48:43 +0000

    Clean duplicated imports

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,9 +1,8 @@
-import { AuthContext, LoginResponse} from '../models';
+import { AuthContext, LoginResponse, LoginRequest} from '../models';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { LoginRequest } from '../models';
 
 @Injectable({
   providedIn: 'root'
